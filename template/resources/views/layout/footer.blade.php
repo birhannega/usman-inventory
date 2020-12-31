f8f8cf75b851b72e13eac435497d063d1e6c9582
birhannega <birhannega844@gmail.com>
2020-12-31 05:26:34 +0300

    second commit

--- template/resources/views/layout/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<footer class="footer">
-  <div class="container-fluid clearfix">
-    <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © 
-     <a href="https://t.me/birhannega" target="_blank">Birhan Nega</a> 2020</span>
-   
-  </div>
-</footer>
